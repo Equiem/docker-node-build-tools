91dbdc2af8b3f827472b28deb19a1da403f99729
James Harvey <james.harvey@equiem.com.au>
2020-01-21 09:27:13 +1100

    Added heroku ci functions

---
+++ bin/heroku-functions.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+function poll {
+  local subject=$1
+  local query=$2
+  local parse=$3
+  local success_status=$4
+
+  local i=0
+  local retries=20
+
+  echo -n "Polling for ${subject} "
+  while [[ $i != $retries ]]
+  do
+    json=$(eval $query)
+    status=$(echo $json | eval $parse)
+    if echo $status | egrep -oq $success_status; then
+      echo -e " => $status\n"
+      return 0
+    else
+      echo -n '.'
+      sleep 5
+      i=$((i+1))
+    fi
+  done
+
+  echo -e " => failed\n"
+  echo -e "\nError response\n$json\n"
+  return 1
+}
+
+function has_released {
+  local app=$1
+
+  poll "successful release" \
+    "heroku releases --json -n 1 -a $app" \
+    "jq -r '.[0].status'" \
+    "succeeded"
+}
+
+function is_running {
+  local app=$1
+
+  poll "running server" \
+    "heroku ps --json -a $app" \
+    "jq -r '.[].state'" \
+    "up"
+}
+
+function has_deployed {
+  local app=$1
+  has_released $app && is_running $app
+}
